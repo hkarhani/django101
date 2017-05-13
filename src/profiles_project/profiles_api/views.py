2e11d9081cdf74e0167c61158cee59b8ea010817
Hassan El Karhani <hkarhani@gmail.com>
2017-05-13 19:43:23 +0400

    Adding first Hello-World api interface

--- src/profiles_project/profiles_api/views.py
+++ src/profiles_project/profiles_api/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
 
+from rest_framework.views import APIView
+from rest_framework.response import Response
+
 # Create your views here.
+class HelloApiView(APIView):
+    """Test API View."""
+
+    def get(self, request, format=None):
+        """Retruns a list of API View features."""
+
+        an_apiview = [
+        'Uses HTTP methods as function(get, post, patch, put, delete)',
+        'It is similar to traditional Django view',
+        'Gives you the most control over your logic',
+        'its mapped manually to URLs', ]
+
+        return Response({'message':'hello!', 'an_apiview': an_apiview})
