8422154508afda7b524009e602293ff8be409d71
Hassan El Karhani <hkarhani@gmail.com>
2017-05-19 20:41:34 +0400

    Added login API.

--- src/profiles_project/profiles_api/urls.py
+++ src/profiles_project/profiles_api/urls.py
@@ -8,8 +8,8 @@ from . import views
 router = DefaultRouter()
 
 router.register('hello-viewset', views.HelloViewSet, base_name='hello-viewset')
-router.register('profile', views.UserProfileViewSet) # No need for base_name as model name is used 
-
+router.register('profile', views.UserProfileViewSet) # No need for base_name as model name is used
+router.register('login', views.loginViewSet, base_name='login')
 # order is important as once matched, it executes the class mapped to it
 urlpatterns = [
     url(r'^hello-view/', views.HelloApiView.as_view()),
--- src/profiles_project/profiles_api/views.py
+++ src/profiles_project/profiles_api/views.py
@@ -8,6 +8,8 @@ from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.authentication import TokenAuthentication
 from rest_framework import filters
+from rest_framework.authtoken.serializers import AuthTokenSerializer
+from rest_framework.authtoken.views import ObtainAuthToken
 
 from . import serializers
 from . import models
@@ -125,4 +127,12 @@ class UserProfileViewSet(viewsets.ModelViewSet):
     permission_classes = (permissions.UpdateOwnProfile, )
     filter_backends = (filters.SearchFilter, )
     search_fields = ('name', 'email', )
+
+class loginViewSet(viewsets.ViewSet):
+    """Checks email and passwor ""d and returns an auth token."""
+
+    serializer_class = AuthTokenSerializer
+    def create(self, request):
+        """ Use the ObtainAuthToken APIView to validate and create a token."""
+        return ObtainAuthToken().post(request)
     
